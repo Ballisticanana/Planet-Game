0ca238e5e8124493c7fcf9a982a51d80e5817892
Ballisticanana <loganwseifert@gmail.com>
2024-10-14 10:02:05 -0600

    version 0.03

---
+++ Planet Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player Body
+    private Rigidbody playerRb;
+
+    //Player controlls
+    public float horizontalInput, forwardInput;
+
+    //Player Settings
+    public float playerSpeed;
+
+    //World info
+    private GameObject gameCenter;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        gameCenter = GameObject.Find("Game Center");
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(gameCenter.transform.forward * forwardInput * playerSpeed * Time.deltaTime);
+        if (!Input.GetKey(KeyCode.LeftShift) && horizontalInput != 0)
+        {
+            playerRb.AddForce(gameCenter.transform.right * horizontalInput * playerSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Planet Game/Assets/Scripts/WeakEnemyScripts.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakEnemyScripts : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    public Vector3 enemyLocal;
+
+    private GameObject player;
+    public Vector3 playerLocal;
+
+    //Enemy 0 name: Basic Ball
+    public float enemy0Speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLocal = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        enemyLocal = new Vector3(transform.position.x, 0, transform.position.z);
+        enemyRb.AddForce((playerLocal - enemyLocal).normalized * enemy0Speed * Time.deltaTime);
+    }
+}
