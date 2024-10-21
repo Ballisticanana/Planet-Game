e8d43d890bfd1105abf5dfa9b249dce692c929a2
Logan S <loganwseifert@gmail.com>
2024-10-21 10:14:34 -0500

    Update SpawnManager.cs

--- Planet Game/Assets/Scripts/SpawnManager.cs
+++ Planet Game/Assets/Scripts/SpawnManager.cs
@@ -30,6 +30,7 @@ public class SpawnManager : MonoBehaviour
             {
                 // Reactivating Impact Particle
                 Debug.Log("Reactivating Impactparticle " + i);
+                // Controls the transportation, activeation & deactivation 
                 StartCoroutine(ImpactParticleReturn(i, impactPoint));
                 // Set bool false stopping next step from Instantiating new game object in pool
                 impactParticleNoAvailableObject = false;
@@ -40,15 +41,20 @@ public class SpawnManager : MonoBehaviour
         // checks bool if Impact Particle was inabled, if not Instantiate new Impact Particle in pool
         if (impactParticleNoAvailableObject == true)
         {
+            // Adds a clone of public game object to the pool
             impactParticlePool.Add(GameObject.Instantiate(impactParticleGameObject, impactPoint, Quaternion.identity));
+            // Controls the transportation, activeation & deactivation
             StartCoroutine(ImpactParticleReturn(impactParticlePool.Count-1, impactPoint));
         }
     }
     IEnumerator ImpactParticleReturn(int spawnQueue, Vector3 impactPoint)
     {
+        // Transfers position to ImpactPoint vector & activates Impact Particle 
         impactParticlePool[spawnQueue].transform.position = impactPoint;
         impactParticlePool[spawnQueue].SetActive(true);
+        // Time between top and bottom Function
         yield return new WaitForSeconds(1);
+        // Transfers position to Spawn Manager position & deactivates Impact Particle
         impactParticlePool[spawnQueue].SetActive(false);
         impactParticlePool[spawnQueue].transform.position = transform.position;
     }
