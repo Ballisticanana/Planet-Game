ec85177873a179c5cb22f70ebe21620fd6760b1d
Logan S <loganwseifert@gmail.com>
2024-10-21 12:27:20 -0500

    Update PlayerController.cs

--- Planet Game/Assets/Scripts/PlayerController.cs
+++ Planet Game/Assets/Scripts/PlayerController.cs
@@ -4,49 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
     //Player Body
-    //Test
-    //Update test
-    //one last time ok lets go
     private Rigidbody playerRb;
 
     //Player controlls
-    public float horizontalInput, forwardInput;
+    private float horizontalInput, forwardInput;
 
-    //Player Settings
+    //Player Setting Variables
     public float playerSpeed;
 
     //World info
     private GameObject gameCenter;
-
-    public GameObject impact;
-    public List<GameObject> testPool;
-    
-
-
+    #endregion
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         gameCenter = GameObject.Find("Game Center");
-
-        testPool = new List<GameObject>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            testPool.Add(GameObject.Instantiate(impact));
-        }
-
+    #region Player W,A,S,D
+        // Assign values to each float
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
+        // Moves player forward and backwards relitive to the game centers faceing direction
         playerRb.AddForce(gameCenter.transform.forward * forwardInput * playerSpeed * Time.deltaTime);
+        // Moves player left and right relitive to the game centers faceing direction & if shift is not pressed
         if (!Input.GetKey(KeyCode.LeftShift) && horizontalInput != 0)
         {
             playerRb.AddForce(gameCenter.transform.right * horizontalInput * playerSpeed * Time.deltaTime);
         }
+    #endregion
     }
 }
