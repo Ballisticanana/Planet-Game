8fe6fae2c635bdd1f176ecef93886925ddff011b
Ballisticanana <loganwseifert@gmail.com>
2024-10-30 10:00:43 -0600

    version 0.0.23

--- Planet Game/Assets/Scripts/PlayerController.cs
+++ Planet Game/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,26 +9,45 @@ public class PlayerController : MonoBehaviour
     #region Variables
     //Player Body
     private Rigidbody playerRb;
+    private Vector3 playerSavedJumpDirection;
 
     //Player controlls
     private float horizontalInput, forwardInput;
+    private bool canJump = true;
 
     //Player Setting Variables
     public float playerSpeed;
     public float jumpStrength;
+    public float jumpCooldownLength;
 
     //World info
     private GameObject gameCenter;
+    public ParticleSystem planetGlow;
+    //ParticleSystem.ColorOverLifetimeModule colorModule;
     #endregion
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         gameCenter = GameObject.Find("Game Center");
+        //planetGlow
     }
 
     void Update()
     {
+        if (canJump == true)
+        {
+            //planetGlow.MainModule.startColor = new Color(95, 255, 0 ,255);
+            //planetGlow.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerSavedJumpDirection = new Vector3(horizontalInput, 0, forwardInput).normalized;
+                playerRb.velocity = Vector3.zero;
+                playerRb.velocity = playerRb.velocity + playerSavedJumpDirection * jumpStrength;
+                StartCoroutine("JumpCooldown");
+            }
+        }
+            
         if (Input.GetKeyDown(KeyCode.T))
         {
             GameObject.Find("Spawn Manager").GetComponent<SpawnManager>().EnemyMoonGameObjectRetrieve(new Vector3(Random.Range(-10,11),0.75f, Random.Range(-10, 11)));
@@ -53,5 +73,12 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Reseting");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    IEnumerator JumpCooldown()
+    {
+        //planetGlow.startColor = new Color(255, 255, 255, 255);
+        canJump = false;
+        yield return new WaitForSeconds(jumpCooldownLength);
+        canJump = true;
     }
 }
